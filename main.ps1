9f7c77788fb29839052235720fec55ec8ead8ed2
Lennart Demes <31403260+StruffelProductions@users.noreply.github.com>
2022-06-08 20:14:04 +0200

    Add proper parameters

--- main.ps1
+++ main.ps1
@@ -1,4 +1,38 @@
-﻿#region Classes
+﻿#region Parameters
+
+param(
+    [String]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Mandatory=$true)]
+    $NotionDatabase,
+
+    [String]
+    [Parameter(Mandatory=$true)]
+    $NotionSecret,
+
+    [System.IO.DirectoryInfo]
+    [ValidateScript({$_.Exists})]
+    $CheckScriptsDirectory = "$PSScriptRoot/checks",
+
+    [Parameter(Mandatory=$true)]
+    [System.IO.DirectoryInfo]
+    [ValidateScript({$_.Exists})]
+    $Directory,
+
+    [String]
+    $DirectoryFilter = '*',
+
+    [String]
+    $NotionPropertyNamePrefix = ".",
+
+    [String]
+    [ValidateNotNullOrEmpty()]
+    $NotionIdFilePath = "/id.notion"
+)
+
+#endregion
+
+#region Classes
 
 class ScriptCheck{
     $ScriptPath
@@ -11,6 +45,8 @@ class ScriptCheck{
 #region Notion data model functions
 function New-NotionDbSchema{
     param(
+        [String]
+        [Parameter(Mandatory=$true)]
         [ValidateSet("Text","Number","Title","Checkbox")]
         $Type
     )
@@ -36,6 +72,7 @@ function New-NotionDbSchema{
 function New-NotionDbProperty{
     param(
         [String]
+        [Parameter(Mandatory=$true)]
         [ValidateSet("Text","Number","Title","Checkbox")]
         $Type,
         $Value
@@ -85,59 +122,58 @@ function New-NotionDbProperty{
 #region Notion API functions
 function Invoke-NotionApiRequest{
     param(
-        [String]$Version,
-        [String]$Endpoint,
+        [Parameter(Mandatory=$true)][String]$Version,
+        [Parameter(Mandatory=$true)][String]$Endpoint,
         $Body,
-        [String]$Secret,
-        [Microsoft.PowerShell.Commands.WebRequestMethod]$Method
+        [Parameter(Mandatory=$true)][String]$Secret,
+        [Parameter(Mandatory=$true)][Microsoft.PowerShell.Commands.WebRequestMethod]$Method
     )
 
     Write-Debug "Sending $Method request to $Endpoint"
     $BodyJson = ($Body | ConvertTo-Json -Depth 100)
-    if($BodyJson -ne $null){
+    if($BodyJson -ne $Null){
         Write-Debug -Message $BodyJson
     }else{
         Write-Debug -Message "`$Body is null."
     }
     
-    Invoke-RestMethod -Headers @{"Authorization" = $secret; "Notion-Version" = $Version; "Content-Type" = "application/json"} -UseBasicParsing -Method $Method -Uri $Endpoint -Body $BodyJson
+    Invoke-RestMethod -Headers @{"Authorization" = $Secret; "Notion-Version" = $Version; "Content-Type" = "application/json"} -UseBasicParsing -Method $Method -Uri $Endpoint -Body $BodyJson
 }
-
 function Update-NotionDbSchema{
     param(
-        [String]$secret,
-        [String]$db,
-        [PSCustomObject]$Schema
+        [Parameter(Mandatory=$true)][String]$Secret,
+        [Parameter(Mandatory=$true)][String]$Database,
+        [Parameter(Mandatory=$true)][PSCustomObject]$Schema
     )
 
     $Body = @{
         "properties" = $Schema
     }
 
-    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/databases/$db" -Secret $secret -Method Patch -Body $Body
+    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/databases/$Database" -Secret $Secret -Method Patch -Body $Body
 }
 
 function New-NotionDbPage{
     param(
-        [String] $secret,
-        [String] $db,
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Database,
         [PSCustomObject]$Properties = @{}
     )
 
     $Body = @{
         "parent" = @{
-            "database_id" = "$db"
+            "database_id" = "$Database"
         }
         "properties" = $Properties
     }
 
-    Invoke-NotionApiRequest -Secret $secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages" -Method Post -Body $Body
+    Invoke-NotionApiRequest -Secret $Secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages" -Method Post -Body $Body
 }
 
 function Update-NotionDbPage{
     param(
-        [String] $secret,
-        [String] $page,
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Page,
         [PSCustomObject]$Properties = @{}
     )
 
@@ -145,49 +181,49 @@ function Update-NotionDbPage{
         "properties" = $Properties
     }
 
-    Invoke-NotionApiRequest -Secret $secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$page" -Method Patch -Body $Body
+    Invoke-NotionApiRequest -Secret $Secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$Page" -Method Patch -Body $Body
 }
 
 function Remove-NotionDbPage{
     param(
-        [String] $secret,
-        [String] $page
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Page
     )
 
     $Body = @{
-        "archived"=$true
+        "archived"=$True
     }
 
-    Invoke-NotionApiRequest -Secret $secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$page" -Method Patch -Body $Body
+    Invoke-NotionApiRequest -Secret $Secret -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$Page" -Method Patch -Body $Body
 }
 
 function Get-NotionDbPage{
     param(
-        [String] $secret,
-        [String] $Id
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Page
     )
     
-    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$Id" -Secret $secret -Method Get
+    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/pages/$Page" -Secret $Secret -Method Get
     
 }
 
 function Get-NotionDb{
     param(
-        [String] $secret,
-        [String] $db
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Database
     )
 
-    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/databases/$db" -Secret $secret -Method Get
+    Invoke-NotionApiRequest -Version "2022-02-22" -Endpoint "https://api.notion.com/v1/databases/$Database" -Secret $Secret -Method Get
 }
 
 function Get-NotionDbChildPages{
     param(
-        [String] $secret,
-        [String] $db
+        [Parameter(Mandatory=$true)][String] $Secret,
+        [Parameter(Mandatory=$true)][String] $Database
     )
     $Body = @{}
     do{
-        $NotionResponse = Invoke-NotionApiRequest -Secret $secret -Version "2022-02-22" -Method Post -Endpoint "https://api.notion.com/v1/databases/$db/query" -Body $Body
+        $NotionResponse = Invoke-NotionApiRequest -Secret $Secret -Version "2022-02-22" -Method Post -Endpoint "https://api.notion.com/v1/databases/$Database/query" -Body $Body
         $NotionResults += $NotionResponse.results
         $Body.start_cursor = $NotionResponse.next_cursor
     }while($NotionResponse.has_more)
@@ -200,27 +236,27 @@ function Get-NotionDbChildPages{
 
 function Register-NotionDirectoryLink {
     param(
-        [System.IO.DirectoryInfo[]]$Directory,
-        [String]$NotionIdFilePath = '/id.notion'
+        [Parameter(Mandatory=$true)][System.IO.DirectoryInfo]$Directory,
+        [Parameter(Mandatory=$true)][String]$NotionIdFilePath
     )
 
     Write-Host -ForegroundColor Green $Directory.Name
-
+    
     $CurrentNotionIdFile="$($_.FullName)$($NotionIdFilePath)"
-    $CurrentNotionId = $null
+    $CurrentNotionId = $Null
 
     if(Test-Path $CurrentNotionIdFile){
         $CurrentNotionId = Get-Content -Path $CurrentNotionIdFile
         try{
-            $ExistingPage = Get-NotionDbPage -secret $secret -Id $CurrentNotionId
+            $ExistingPage = Get-NotionDbPage -Secret $NotionSecret -Page $CurrentNotionId
             if($ExistingPage.archived){
-                $CurrentNotionId = $null
+                $CurrentNotionId = $Null
                 Write-Host "A linked notion page already exists, but it is archived."
             }else{
                 Write-Host "A linked notion page already exists."
             }
         }catch{
-            $CurrentNotionId = $null
+            $CurrentNotionId = $Null
             Remove-Item $CurrentNotionIdFile
             Write-Host  "A linked notion page could not be located. ($($_.Exception.Message))"
         }
@@ -229,7 +265,7 @@ function Register-NotionDirectoryLink {
     if(-Not $CurrentNotionId){
         [PSCustomObject]$NewProperties = @{}
         $NewProperties.Name = (New-NotionDbProperty -Type Title -Value $Directory.Name )
-        $NotionResponse = New-NotionDbPage -secret $secret -db $db -properties $NewProperties
+        $NotionResponse = New-NotionDbPage -Secret $NotionSecret -Database $NotionDatabase -properties $NewProperties
         Write-Debug -Message $NotionResponse
         $CurrentNotionId = $NotionResponse.id
         Write-Host "A new linked notion page has been created: $CurrentNotionId"
@@ -241,19 +277,12 @@ function Register-NotionDirectoryLink {
 }
 #endregion
 
-$db = "b103cdddd45d43dda84e8cff956d137e"
-$secret = Get-Content "$PSScriptRoot/token.secret"
-$ChecksFolder = "$PSScriptRoot/checks"
-$TargetFolder = "T:\WIP"
-$Filter = '*'
-$NotionPropertyNamePrefix = "."
-
 #region Initialize
 
-$TargetDirectories = Get-ChildItem -Directory -Path $TargetFolder -Filter $Filter
+$TargetDirectories = Get-ChildItem -Directory -Path $Directory -Filter $Filter
 
 [ScriptCheck[]]$Checks = @()
-Get-ChildItem -Path $ChecksFolder -File -Filter '*.ps1' | ForEach-Object{
+Get-ChildItem -Path $CheckScriptsDirectory -File -Filter '*.ps1' | ForEach-Object{
     $Checks += [ScriptCheck]@{
         "ScriptPath"=$_.FullName;
         "Name"=$_.BaseName.split('-')[1];
@@ -282,9 +311,9 @@ $Checks | ForEach-Object{
 
 $OldDbSchema = @{}
 
-$NotionDb = Get-NotionDb -secret $secret -db $db
+$NotionDb = Get-NotionDb -Secret $NotionSecret -Database $NotionDatabase
 $NotionDb.properties | Get-Member | Where-Object{$_.Name -like "$NotionPropertyNamePrefix*"} | ForEach-Object{
-    $OldDbSchema.Add($_.Name,$null)
+    $OldDbSchema.Add($_.Name,$Null)
 }
 
 # Replace new schema into old schema
@@ -295,7 +324,7 @@ $NewDbSchema.Keys | ForEach-Object{
     $UpdateSchema[$_] = $NewDbSchema[$_]
 }
 
-Update-NotionDbSchema -secret $secret -db $db -Schema $UpdateSchema
+Update-NotionDbSchema -Secret $NotionSecret -Database $NotionDatabase -Schema $UpdateSchema
 
 #endregion
 
@@ -304,7 +333,7 @@ $NotionPageIds = @()
 
 $TargetDirectories | ForEach-Object{
     $CurrentDirectory = $_
-    $CurrentNotionId = (Register-NotionDirectoryLink -Directory $_)
+    $CurrentNotionId = (Register-NotionDirectoryLink -Directory $_ -NotionIdFilePath $NotionIdFilePath)
     $NotionPageIds += $CurrentNotionId
 
     $NewProperties = @{}
@@ -316,27 +345,27 @@ $TargetDirectories | ForEach-Object{
             $Result = (& $CurrentCheck.ScriptPath $CurrentDirectory.FullName | Select-Object -Last 1)
         }catch{
             Write-Warning "'$($CurrentCheck.ScriptPath)' for directory '$CurrentDirectory' could not be processed: $_"
-            $Result = $false
+            $Result = $False
         }
         Write-Host -ForegroundColor Cyan "$($CurrentCheck.Name.PadRight($LongestCheckNameLength+3,' ')): $Result"
         $NewProperties["$NotionPropertyNamePrefix$($CurrentCheck.Name)"] = (New-NotionDbProperty -Type $_.Type -Value $Result)
         
     }
     Write-Debug ($NewProperties | ConvertTo-Json -Depth 100)
-    $NotionResponse = Update-NotionDbPage -secret $secret -page $CurrentNotionId -properties $NewProperties
+    $NotionResponse = Update-NotionDbPage -Secret $NotionSecret -page $CurrentNotionId -properties $NewProperties
     Write-Debug -Message ($NotionResponse | ConvertTo-Json -Depth 100)
 }
 #endregion
 
 #region Delete Orphaned Notion Pages
 
-$RemotePages = Get-NotionDbChildPages -secret $secret -db $db
+$RemotePages = Get-NotionDbChildPages -Secret $NotionSecret -Database $NotionDatabase
 
 $PagesToDelete = $RemotePages | Where-Object {$_.id -notin $NotionPageIds}
-Write-Host "$(($PagesToDelete|Measure-Object).Count) pages are no longer referenced by a directory and will be deleted."
+Write-Host "$(($PagesToDelete|Measure-Object).Count) pages are no longer referenced by a directory and will be deleted..."
 
 $PagesToDelete | ForEach-Object{
-    $NotionResponse = Remove-NotionDbPage -secret $secret -page $_.id
+    $NotionResponse = Remove-NotionDbPage -Secret $NotionSecret -page $_.id
     Write-Debug -Message ($NotionResponse | ConvertTo-Json -Depth 100)
 }
 #endregion
